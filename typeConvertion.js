0c71c7004fb10ee306ec89c49c06ce2df57717c5
Muhammad Khairul Anwar <khairul240102@gmail.com>
2024-03-21 23:56:16 +0700

    typeConvertion

---
+++ typeConvertion.js
@@ -0,0 +1,8 @@
+function printSum(string1, string2) {
+    // Write your code below this line
+    let num1 = Number(string1);
+    let num2 = Number(string2);
+    const sum = num1 + num2;
+    console.log(sum);
+
+}
